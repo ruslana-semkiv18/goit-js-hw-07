1c8be235bfe579c5c34c9c80976f144bc5df5e32
ruslana-semkiv18 <semkivruslana18@gmail.com>
2024-06-30 20:05:36 +0300

    done task-4

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,23 @@
+const form = document.querySelector(".login-form");
 
+form.addEventListener("submit", handlerSubmit);
+function handlerSubmit(evt) {
+    evt.preventDefault();
+
+    const {email, password} = evt.currentTarget.elements;
+    const emailValue = email.value.trim();
+    const passwordValue = password.value.trim();
+
+    if (!emailValue || !passwordValue) {
+        alert("All form fields must be filled in");
+        return;
+    }
+    
+    const data = {
+        email: emailValue,
+        password: passwordValue,
+    }
+
+    console.log(data);
+    form.reset();
+}
