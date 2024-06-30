3e716356fae69a68779d681362e4ccea6752721a
ruslana-semkiv18 <semkivruslana18@gmail.com>
2024-06-30 20:32:03 +0300

    done task-5

--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,16 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+
+const changeColor = document.querySelector(".change-color");
+const spanColor = document.querySelector(".color");
+const bodyColor = document.body;
+
+changeColor.addEventListener("click", handlerClick);
+
+function handlerClick() {
+  const color = getRandomHexColor();
+  bodyColor.style.backgroundColor = color;
+  spanColor.textContent = color;
+}
