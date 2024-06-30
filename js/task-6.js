a1199049dc49122a9514c83313626badf7d2e9a5
ruslana-semkiv18 <semkivruslana18@gmail.com>
2024-06-30 22:18:51 +0300

    done task-6

--- js/task-6.js
+++ js/task-6.js
@@ -3,3 +3,35 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const inputNumber = document.querySelector('input[type="number"]');
+const buttonCreate = document.querySelector("button[data-create]");
+const buttonDestroy = document.querySelector("button[data-destroy]");
+const boxesContainer = document.querySelector("#boxes");
+
+
+buttonCreate.addEventListener("click", createBoxes);
+
+function createBoxes() {
+  const number = inputNumber.value;
+  if (number >= 1 && number <= 100) {
+    boxesContainer.innerHTML = "";
+    let size = 30;
+    for (let i = 0; i < number; i++) {
+      const box = document.createElement("div");
+      box.style.width = `${size}px`;
+      box.style.height = `${size}px`;
+      box.style.backgroundColor = getRandomHexColor();
+      boxesContainer.appendChild(box);
+      size += 10;
+    }
+    inputNumber.value = "";
+  } 
+}
+
+buttonDestroy.addEventListener("click", destroyBoxes);
+
+function destroyBoxes() {
+  boxesContainer.innerHTML = "";
+}
+   
